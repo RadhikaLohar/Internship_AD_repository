b3e00fbf194ea6de0a37e798b67f809e9b9e0f2e
RadhikaLohar <98337205+RadhikaLohar@users.noreply.github.com>
2022-03-21 09:51:13 +0530

    Add files via upload

---
+++ Assignment1/src/com/mastek/demo/model/copy/Rectangle.java
@@ -0,0 +1,10 @@
+package com.mastek.demo.model.copy;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+        System.out.println("Shape: Rectangle");
+	}
+
+}
---
+++ Assignment1/src/com/mastek/demo/model/copy/Shape.java
@@ -0,0 +1,5 @@
+package com.mastek.demo.model.copy;
+
+public interface Shape {
+	void draw();
+}
